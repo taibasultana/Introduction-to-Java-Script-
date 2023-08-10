7e63590726ed60bf978bd65397199581f2be3e83
taibasultana <tsmtaibasultana@gmail.com>
2023-08-10 20:11:25 +0600

    done

---
+++ convesion.js
@@ -0,0 +1,27 @@
+var price1 = 31 ;
+var price2 = 10 ;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var firstName = "jennie";
+var lastName = "Kim";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+
+// convert string  to number
+console.log(price1);
+var priceName = parseInt(price1);
+console.log(priceNumber);
+// 
+
+var gpa = '3.41';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
+
---
+++ summary.js
@@ -0,0 +1,10 @@
+// *** Java Script - interpreting high level script language. more interactiove in webpage
+// * variable type - number , string, boolean, null, undefined
+// * reserved word like true, false, return etc
+// * camel case , snake case, pascal case
+//  varible names case sensative capital & small letter
+// *operation: + - * / % 
+//* shorthand: ++ -- += -= *= /=
+// *string er majhe = diye inverted coma diye khali rakhle string er majhe gap hoi
+// * string to intstring to float , fixed 
+// * reminder
